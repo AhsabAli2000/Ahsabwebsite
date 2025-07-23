58f36b96f8ef11654ac2b42f5b9cbfbf60b24554
AhsabAli2000 <ahsabalianan2000@gmail.com>
2025-07-24 01:41:44 +0600

    No major change to server logic. Only frontend slight style changes and content change

--- app.js
+++ app.js
@@ -1,59 +1,71 @@
-import { Application, send, isHttpError } from "https://deno.land/x/oak@v12.2.0/mod.ts";
+import {
+  Application,
+  isHttpError,
+  send,
+} from "https://deno.land/x/oak@v12.2.0/mod.ts";
 import { exists } from "https://deno.land/std@0.183.0/fs/mod.ts";
-import dot from "https://esm.sh/dot@1.1.3"
-import pagedata from "./pagedata.json" assert { type: "json" }
+import dot from "https://esm.sh/dot@1.1.3";
+import pagedata from "./pagedata.json" with { type: "json" };
 
 const PORT = 8000;
 
 const app = new Application();
-const templ = dot.template(await Deno.readTextFile("www/layout.html"))
+const templ = dot.template(await Deno.readTextFile("www/layout.html"));
 let title, desc;
 
+async function detpath(path) {
+    if (await exists(`www${path}`) && path != "/") path = `www${path}`;
+    else if (await exists(`www${path}.html`)) path = `www${path}.html`;
+    else if (path == "/") path = `www/index.html`;
+    else path = `www${path}`;
+
+    return path;
+}
+
 app.use(async (ctx, next) => {
-	try {
-		await next()
-	} catch (e) {
-		if (isHttpError(e)) {
-			ctx.response.status = e.status
-		} else {
-			ctx.response.status = 500
-		}
-		ctx.response.body = templ({body: `<h1>${ctx.response.status}</h1><p><a href="./index">Back to main site</a></p>`, title: ctx.response.status, desc: ctx.response.status})
-	}
-})
+  try {
+    await next();
+  } catch (e) {
+    if (isHttpError(e)) {
+      ctx.response.status = e.status;
+    } else {
+      ctx.response.status = 500;
+    }
+    ctx.response.body = templ({
+      body:
+        `<h1>${ctx.response.status}</h1><p><a href="./index">Back to main site</a></p>`,
+      title: ctx.response.status,
+      desc: ctx.response.status,
+    });
+  }
+});
 
 app.use(async (ctx) => {
+  const regex = new RegExp("\.htm|\.html");
+  let filename = ctx.request.url.pathname;
+  let nolayout = ["www/google87c048ae2fb1f3da.html"];
 
-	const regex = new RegExp("\.htm|\.html")
-	let filename = ctx.request.url.pathname
+  let sendpath = await detpath(filename);
 
-	let nolayout = ["www/google87c048ae2fb1f3da.html"]
-	
-	async function detpath(path) {
-		if (await exists(`www${path}`) && path != "/") path = `www${path}`
-		else if (await exists(`www${path}.html`)) path = `www${path}.html`
-		else if (path == "/") path = `www/index.html`
-		else path = `www${path}`
-		return path
-	}
+  if (regex.test(sendpath) && !nolayout.includes(sendpath)) {
+    title, desc = filename;
+    for (let i = 0; i < pagedata.length; i++) {
+      if (pagedata[i].name == filename) {
+        title = pagedata[i].title;
+        desc = pagedata[i].desc;
+      }
+    }
 
-	let sendpath = await detpath(filename)
-
-	if (regex.test(sendpath) && !nolayout.includes(sendpath)){
-		title, desc = filename
-		for (let i = 0; i < pagedata.length; i++) {
-			if (pagedata[i].name == filename) {
-				title = pagedata[i].title
-				desc = pagedata[i].desc
-			}
-		}
-
-	ctx.response.body = await templ({body: await Deno.readTextFile(sendpath), title: title, desc: desc})
-	} else {
-		await send(ctx, sendpath, {
-			root: "./",
-		})
-	}
+    ctx.response.body = await templ({
+      body: await Deno.readTextFile(sendpath),
+      title: title,
+      desc: desc,
+    });
+  } else {
+    await send(ctx, sendpath, {
+      root: "./",
+    });
+  }
 });
 
 app.listen({
